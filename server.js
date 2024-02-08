4ff3cebd6746674af89c5d8d749bb168842524c9
Juri Vgr. <yuravegera@gmail.com>
2024-02-08 15:42:17 +0200

    Hello World

---
+++ server.js
@@ -0,0 +1,13 @@
+//console.log('Hello, World!');
+
+let firstName =  'Yura';
+let lastName = 'Vgr';
+
+//console.log(firstName);
+
+console.log('Hello' +firstName+'!');
+
+console.log(`Hello, ${firstName}!`)
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
